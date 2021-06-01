a8f43ea7537e0008dd2717009474078c87461e58
PlatinPython <robert.alexander.kaempf@gmail.com>
2021-06-01 21:07:06 +0200

    Added Dispenser Behaviour

--- src/main/java/platinpython/rgbblocks/dispenser/DispensePaintbucketBehaviour.java
+++ src/main/java/platinpython/rgbblocks/dispenser/DispensePaintbucketBehaviour.java
@@ -1,7 +1,30 @@
 package platinpython.rgbblocks.dispenser;
 
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import platinpython.rgbblocks.tileentity.RGBTileEntity;
 
 public class DispensePaintbucketBehaviour extends DefaultDispenseItemBehavior {
-	
+	@Override
+	protected ItemStack execute(IBlockSource source, ItemStack itemStack) {
+		Direction dispenserFacing = source.getBlockState().getValue(DispenserBlock.FACING);
+		BlockPos blockPos = source.getPos().relative(dispenserFacing);
+		TileEntity tileEntity = source.getLevel().getBlockEntity(blockPos);
+		if(tileEntity instanceof RGBTileEntity) {
+			((RGBTileEntity) tileEntity).setColor(itemStack.getTag().getInt("color"));
+			source.getLevel().sendBlockUpdated(blockPos, tileEntity.getBlockState(), tileEntity.getBlockState(), -1);
+			return itemStack;
+		} else {
+			IPosition iposition = DispenserBlock.getDispensePosition(source);
+			ItemStack spawnStack = itemStack.split(1);
+			spawnItem(source.getLevel(), spawnStack, 6, dispenserFacing, iposition);
+			return itemStack;
+		}
+	}
 }
