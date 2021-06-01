5db8c66de3b2f575d03a723f43faba51fb32ac26
PlatinPython <robert.alexander.kaempf@gmail.com>
2021-06-01 15:44:59 +0200

    Changed order of blocks

--- src/main/java/platinpython/rgbblocks/util/registries/BlockRegistry.java
+++ src/main/java/platinpython/rgbblocks/util/registries/BlockRegistry.java
@@ -21,52 +21,53 @@ import platinpython.rgbblocks.util.RegistryHandler;
 public class BlockRegistry {
 	public static final RegistryObject<Block> RGB_CONCRETE = register("concrete",
 			() -> new GenericRGBBlock(Properties.copy(Blocks.WHITE_CONCRETE)), false);
+	public static final RegistryObject<Block> RGB_CONCRETE_SLAB = register("concrete_slab",
+			() -> new GenericRGBSlabBlock(Properties.copy(Blocks.WHITE_CONCRETE)), false);
 	public static final RegistryObject<Block> RGB_CONCRETE_STAIRS = register("concrete_stairs",
 			() -> new GenericRGBStairsBlock(() -> RGB_CONCRETE.get().defaultBlockState(),
 					Properties.copy(Blocks.WHITE_CONCRETE)),
 			false);
-	public static final RegistryObject<Block> RGB_CONCRETE_SLAB = register("concrete_slab",
-			() -> new GenericRGBSlabBlock(Properties.copy(Blocks.WHITE_CONCRETE)), false);
 
 	public static final RegistryObject<Block> RGB_CONCRETE_POWDER = register("concrete_powder",
 			RGBConcretePowderBlock::new, false);
 
 	public static final RegistryObject<Block> RGB_WOOL = register("wool",
 			() -> new GenericRGBBlock(Properties.copy(Blocks.WHITE_WOOL)), false);
-	public static final RegistryObject<Block> RGB_WOOL_STAIRS = register("wool_stairs",
-			() -> new GenericRGBStairsBlock(() -> RGB_WOOL.get().defaultBlockState(), Properties.copy(Blocks.WHITE_WOOL)),
-			false);
 	public static final RegistryObject<Block> RGB_WOOL_SLAB = register("wool_slab",
 			() -> new GenericRGBSlabBlock(Properties.copy(Blocks.WHITE_WOOL)), false);
+	public static final RegistryObject<Block> RGB_WOOL_STAIRS = register("wool_stairs",
+			() -> new GenericRGBStairsBlock(() -> RGB_WOOL.get().defaultBlockState(),
+					Properties.copy(Blocks.WHITE_WOOL)),
+			false);
 
 	public static final RegistryObject<Block> RGB_CARPET = register("carpet", RGBCarpetBlock::new, false);
 
 	public static final RegistryObject<Block> RGB_PLANKS = register("planks",
 			() -> new GenericRGBBlock(Properties.copy(Blocks.BIRCH_PLANKS)), false);
+	public static final RegistryObject<Block> RGB_PLANKS_SLAB = register("planks_slab",
+			() -> new GenericRGBSlabBlock(Properties.copy(Blocks.BIRCH_PLANKS)), false);
 	public static final RegistryObject<Block> RGB_PLANKS_STAIRS = register("planks_stairs",
 			() -> new GenericRGBStairsBlock(() -> RGB_PLANKS.get().defaultBlockState(),
 					Properties.copy(Blocks.BIRCH_PLANKS)),
 			false);
-	public static final RegistryObject<Block> RGB_PLANKS_SLAB = register("planks_slab",
-			() -> new GenericRGBSlabBlock(Properties.copy(Blocks.BIRCH_PLANKS)), false);
 
 	public static final RegistryObject<Block> RGB_TERRACOTTA = register("terracotta",
 			() -> new GenericRGBBlock(Properties.copy(Blocks.TERRACOTTA)), false);
+	public static final RegistryObject<Block> RGB_TERRACOTTA_SLAB = register("terracotta_slab",
+			() -> new GenericRGBSlabBlock(Properties.copy(Blocks.TERRACOTTA)), false);
 	public static final RegistryObject<Block> RGB_TERRACOTTA_STAIRS = register("terracotta_stairs",
 			() -> new GenericRGBStairsBlock(() -> RGB_TERRACOTTA.get().defaultBlockState(),
 					Properties.copy(Blocks.TERRACOTTA)),
 			false);
-	public static final RegistryObject<Block> RGB_TERRACOTTA_SLAB = register("terracotta_slab",
-			() -> new GenericRGBSlabBlock(Properties.copy(Blocks.TERRACOTTA)), false);
 
 	public static final RegistryObject<Block> RGB_GLASS = register("glass",
 			() -> new GenericRGBGlassBlock(Properties.copy(Blocks.GLASS)), false);
+	public static final RegistryObject<Block> RGB_GLASS_SLAB = register("glass_slab",
+			() -> new GenericRGBGlassSlabBlock(Properties.copy(Blocks.GLASS)), false);
 	public static final RegistryObject<Block> RGB_GLASS_STAIRS = register("glass_stairs",
 			() -> new GenericRGBGlassStairsBlock(() -> RGB_GLASS.get().defaultBlockState(),
 					Properties.copy(Blocks.GLASS)),
 			false);
-	public static final RegistryObject<Block> RGB_GLASS_SLAB = register("glass_slab",
-			() -> new GenericRGBGlassSlabBlock(Properties.copy(Blocks.GLASS)), false);
 
 	public static ArrayList<Block> nonGlowingBlocks = new ArrayList<>();
 
