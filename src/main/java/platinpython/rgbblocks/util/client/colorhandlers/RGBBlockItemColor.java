d2d9cd3457cf87b0239fbd67fb2d6c735d61061a
PlatinPython <robert.alexander.kaempf@gmail.com>
2021-06-09 17:16:28 +0200

    Removed remaining OnlyIn

--- src/main/java/platinpython/rgbblocks/util/client/colorhandlers/RGBBlockItemColor.java
+++ src/main/java/platinpython/rgbblocks/util/client/colorhandlers/RGBBlockItemColor.java
@@ -3,11 +3,8 @@ package platinpython.rgbblocks.util.client.colorhandlers;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import platinpython.rgbblocks.item.RGBBlockItem;
 
-@OnlyIn(Dist.CLIENT)
 public class RGBBlockItemColor implements IItemColor {
 	public int getColor(ItemStack stack, int tintindex) {
 		if (stack.getItem() instanceof RGBBlockItem) {
