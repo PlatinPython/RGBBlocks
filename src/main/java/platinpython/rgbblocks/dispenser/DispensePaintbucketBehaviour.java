b17d04714791242f88d594ebdbe276a0854d3cd9
PlatinPython <robert.alexander.kaempf@gmail.com>
2021-06-09 10:51:23 +0200

    Removed smoke particles from Dispenser when 'painting' blocks

--- src/main/java/platinpython/rgbblocks/dispenser/DispensePaintbucketBehaviour.java
+++ src/main/java/platinpython/rgbblocks/dispenser/DispensePaintbucketBehaviour.java
@@ -27,4 +27,8 @@ public class DispensePaintbucketBehaviour extends DefaultDispenseItemBehavior {
 			return itemStack;
 		}
 	}
+	
+	@Override
+	protected void playAnimation(IBlockSource source, Direction direction) {
+	}
 }
