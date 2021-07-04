61f3cd6e9ee76e635f5002f890e79f60aa0feee0
PlatinPython <robert.alexander.kaempf@gmail.com>
2021-07-04 23:15:05 +0200

    'Fixed' data generator errors

--- src/main/java/platinpython/rgbblocks/data/DataGatherer.java
+++ src/main/java/platinpython/rgbblocks/data/DataGatherer.java
@@ -1,5 +1,7 @@
 package platinpython.rgbblocks.data;
 
+import java.util.Collections;
+
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -7,7 +9,7 @@ import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 public class DataGatherer {
 	public static void onGatherData(GatherDataEvent event) {
 		DataGenerator generator = event.getGenerator();
-		ExistingFileHelper helper = event.getExistingFileHelper();
+		ExistingFileHelper helper = new ExistingFileHelper(Collections.emptyList(), Collections.emptySet(), false);
 
 		if (event.includeClient()) {
 			generator.addProvider(new ModLanguageProvider(generator));
