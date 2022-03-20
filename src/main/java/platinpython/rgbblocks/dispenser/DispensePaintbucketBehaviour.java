effc1a70793bd9a10f75eea3ddccea05ad892abd
PlatinPython <robert.alexander.kaempf@gmail.com>
2022-03-20 01:40:46 +0100

    Fix issues with durability.

--- src/main/java/platinpython/rgbblocks/dispenser/DispensePaintbucketBehaviour.java
+++ src/main/java/platinpython/rgbblocks/dispenser/DispensePaintbucketBehaviour.java
@@ -5,6 +5,7 @@ import net.minecraft.core.BlockSource;
 import net.minecraft.core.Direction;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -16,14 +17,22 @@ public class DispensePaintbucketBehaviour extends DefaultDispenseItemBehavior {
         Direction dispenserFacing = source.getBlockState().getValue(DispenserBlock.FACING);
         BlockPos blockPos = source.getPos().relative(dispenserFacing);
         BlockEntity tileEntity = source.getLevel().getBlockEntity(blockPos);
-        if (tileEntity instanceof RGBTileEntity) {
-            ((RGBTileEntity) tileEntity).setColor(itemStack.getTag().getInt("color"));
+        if (tileEntity instanceof RGBTileEntity rgbTileEntity) {
+            boolean broke = false;
+            if (itemStack.getOrCreateTag().getInt("color") != rgbTileEntity.getColor()) {
+                if (itemStack.getDamageValue() == itemStack.getMaxDamage() - 1) {
+                    broke = true;
+                } else {
+                    itemStack.hurt(1, source.getLevel().random, null);
+                }
+            }
+            rgbTileEntity.setColor(itemStack.getOrCreateTag().getInt("color"));
             source.getLevel()
                   .sendBlockUpdated(blockPos,
                                     tileEntity.getBlockState(),
                                     tileEntity.getBlockState(),
                                     Block.UPDATE_ALL_IMMEDIATE);
-            return itemStack;
+            return broke ? new ItemStack(Items.BUCKET) : itemStack;
         } else {
             return super.execute(source, itemStack);
         }
